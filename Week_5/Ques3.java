691205fecefc131da7461cbc4b0c54d98ec72874
Mohd Sohaib <160093489+sohaibazmi101@users.noreply.github.com>
2024-08-30 11:42:40 +0000

    initial commit

--- Week_5/Ques3.java
+++ Week_5/Ques3.java
@@ -5,19 +5,21 @@ import java.util.Scanner;
 public class Ques3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num1, num2;
-        System.out.println("Enter the range of numbers : ");
-        num1 = scanner.nextInt();
-        num2 = scanner.nextInt();
-        System.out.print("Prime numbers Between " + num1 + " and " + num2 + " : ");
+        int lb, ub;
+        System.out.print("Enter the lower bound of range : ");
+        lb = scanner.nextInt();
+        System.out.print("Enter the upper bound of range : ");
+        ub = scanner.nextInt();
+        for(int i = lb; i <= ub; i++){
+            if(isPrime(i)){
+                System.out.print(i+" ");
+            }
+        }
         scanner.close();
     }
 
-    public static void printPrime(int num1,int num2){
-        System.out.println(num1+num2);
-    }
     public static boolean isPrime(int num){
-        if(i <= 1){
+        if(num <= 1){
             return false;
         }
         for (int i = 2; i <= Math.sqrt(num); i++) {
